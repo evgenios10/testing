9b33dec3940ad23f08be8882038479289c249c03
evgenios10 <evgenioshadjipetrou2@gmail.com>
2024-02-26 21:55:36 +0200

    Updated helo.c with new functionality

--- helo.c
+++ helo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello,Beautiful World!");
+   printf("Hello,Awsome Beautiful World!");
    return 0;
 }
