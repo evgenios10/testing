0d99f0b9c5ea49cd133ee37c02eccbf5be84878e
evgenios10 <evgenioshadjipetrou2@gmail.com>
2024-02-27 21:01:56 +0200

    up

---
+++ reversing.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include <stdlib.h> 
+#include<time.h> 
+
+int main(){
+
+int size;
+printf("give the size of the table\n");
+scanf("%d",&size);
+
+srand(time(NULL));
+int arr[size];
+int temp=0;
+int element;
+int check=0;
+
+for(int i=0; i<size; i++){
+    check =0;
+    
+    
+
+    while(check == 0){
+    element=rand();
+    if(element<100)
+        check++;
+    }
+
+    arr[i]=element;
+    printf("%d  ",arr[i]);
+
+}
+printf("\n");
+
+int count=0;
+
+while(count!=size/2 ){
+    temp=arr[count];
+    arr[count]=arr[size-count-1];
+    arr[size-count-1]=temp;
+
+    count++;
+
+}
+
+for(int i=0; i<size; i++){
+
+    printf("%d  ",arr[i]);
+    
+}
+
+return 0;
+
+}
